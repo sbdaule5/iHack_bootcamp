c41d2fce5abbba9eb8351146ef3cfda6c0c5198e
Sanigma <srdandge2003@gmail.com>
2022-10-16 13:28:52 +0530

    added info console and can get input

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from ctypes import resize
+from ctypes import alignment, resize
 import sys
 from tkinter import *
 from tkinter import ttk
@@ -62,11 +62,30 @@ def createEditor(fileContent):
 
     widgetInfoDict["canvas"].pack()
     
-    #Creating a Scrollbar in y and x axis
-    my_vscrollbar = ttk.Scrollbar(widgetInfoDict["lf2"],orient=VERTICAL,command=widgetInfoDict["canvas"].yview)
-    my_vscrollbar.pack(side=RIGHT,fill=Y,expand=True)
+    #Creating a text console
+    # def update():
+    #     print("Hello World")
 
-    my_hscrollbar = ttk.Scrollbar(widgetInfoDict["lf2"],orient=HORIZONTAL,command=widgetInfoDict["canvas"].xview)
+    def info(text):
+        widgetInfoDict["text1"].focus_set()
+        widgetInfoDict["text1"].config(state=NORMAL)
+        #widgetInfoDict["text1"].delete(0,END)
+        text += '\n'
+        widgetInfoDict["text1"].insert('1.0', text)
+        widgetInfoDict["text1"].config(state=DISABLED)
+
+    widgetInfoDict["txtcon"] = LabelFrame(widgetInfoDict["root"],text="Info Console")
+    widgetInfoDict["txtcon"].pack(expand='yes',side='bottom',fill=BOTH)
+    widgetInfoDict["text1"] = Text(widgetInfoDict["txtcon"])
+    widgetInfoDict["text1"].config(state=DISABLED)
+    widgetInfoDict["text1"].pack(fill=BOTH,expand=True)
+    info('Hi')
+    info('Hello')
+    #Creating a Scrollbar in y and x axis
+    my_vscrollbar = ttk.Scrollbar(widgetInfoDict["canvas"],orient=VERTICAL,command=widgetInfoDict["canvas"].yview)
+    my_vscrollbar.pack(anchor=W,fill=Y,expand=True)
+
+    my_hscrollbar = ttk.Scrollbar(widgetInfoDict["canvas"],orient=HORIZONTAL,command=widgetInfoDict["canvas"].xview)
     my_hscrollbar.pack(side=LEFT,fill=X,expand=True)
 
     #Creating Canvas Zoom
@@ -78,10 +97,10 @@ def createEditor(fileContent):
         widgetInfoDict["canvas"].scale('all',0,0,2,2)
 
     btn1 = Button(widgetInfoDict["lf1"],text="Zoom ++",bd=5,command= lambda: size_big())
-    btn1.pack(side='bottom')
+    btn1.pack(side='left')
 
     btn2 = Button(widgetInfoDict["lf1"],text="Zoom --",bd=5,command= lambda: size_small())
-    btn2.pack(side='bottom')
+    btn2.pack(side='left')
 
     widgetInfoDict["pw"].add(widgetInfoDict["lf1"])
     widgetInfoDict["pw"].add(widgetInfoDict["lf2"])
@@ -181,7 +200,6 @@ def renderSnake(coords, name):
     # Add the shape name to the rendered name
     if name:
         widgetInfoDict["canvas"].create_text((coords[0] + coords[2]) / 2, (coords[1] + coords[3]) / 2, text=name)
-
 
 def renderLadder(coords, name):
     if coinsPresenceInfo[coords[0] * BOARD_SIZE + coords[1]]:
